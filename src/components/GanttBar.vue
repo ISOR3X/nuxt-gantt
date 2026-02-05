b6b03e579645cf227a945cb21093ed5edf450c20
ISOR3X <isorex@icloud.com>
2026-02-05 22:15:34 +0100

    chore: explore style for 0day tasks

--- src/components/GanttBar.vue
+++ src/components/GanttBar.vue
@@ -109,8 +109,8 @@ function onMouseUp() {
         <div class="h-full rounded-full border-accented group-hover:border-2" />
       </div>
       <div
-        :class="cursorStyle"
         class="group relative h-full rounded-md border-2 border-primary bg-primary/10 select-none"
+        :class="[{'bevel': Temporal.PlainDate.compare(task.startDate, task.endDate) == 0}, cursorStyle]"
         @mousedown="onMouseDownBar"
       >
         <div
