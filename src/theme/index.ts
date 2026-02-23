b54651c9e20a26f4f1d6d2b8e07bf8207cf7a4b5
ISOR3X <isorex@icloud.com>
2026-02-23 18:37:13 +0100

    chore: format and add index file for theme

---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+export { default as chart } from "./chart";
+export { default as colItem } from "./col-item";
+export { default as dependencyArrow } from "./dependency-arrow";
+export { default as eventMarker } from "./event-marker";
+export { default as gridPattern } from "./grid-pattern";
+export { default as offDayPattern } from "./off-day-pattern";
+export { default as rowItem } from "./row-item";
+export { default as taskBar } from "./task-bar";
